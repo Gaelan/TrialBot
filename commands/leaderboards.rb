4645bbba38e45e7f4f940b408037a6dfa5135657
Gaelan Steele <gbs@canishe.com>
2017-02-01 22:12:24 -0800

    !tr

--- commands/leaderboards.rb
+++ commands/leaderboards.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require_relative 'accounts'
 
 def leaderboards
 	doc = Nokogiri::HTML(open('http://www.blankmediagames.com/Trial/fetch.php?from=leaderboards.php'))
@@ -12,19 +12,28 @@ def leaderboards
 end
 
 Bot.command :toptr do |event, count = 10|
-	begin
-		if count > 25
-			event.respond ":x: Only up to 25 entries can be shown."
+	event.channel.start_typing
+	if count > 25
+		event.respond ":x: Only up to 25 entries can be shown."
+		return
+	end
+
+	text = leaderboards.take(count).map.each_with_index do |(name, tr), index|
+		"#{index + 1}. **#{name}**: #{tr}"
+	end.join("\n")
+
+	event.channel.send_message '', false, Discordrb::Webhooks::Embed.new(title: "Top #{count} Jurors", description: text)
+end
+
+Bot.command :tr do |event|
+	event.channel.start_typing
+	authenticate event do |user|
+		board = leaderboards
+		rank = board.find_index { |name, _| name == user.tos_name }
+		unless rank
+			event.respond ":stuck_out_tongue: You're not even on the leaderboard."
 			return
 		end
-
-		text = leaderboards.take(count).map.each_with_index do |(name, tr), index|
-			"#{index + 1}. **#{name}**: #{tr}"
-		end.join("\n")
-
-		event.channel.send_message '', false, Discordrb::Webhooks::Embed.new(title: "Top #{count} Jurors", description: text)
-
-	rescue => err
-		binding.pry
+		event.respond "**#{user.tos_name}** is in **#{rank.ordinalize}** place with a Trial Rank of **#{board[user.tos_name]}**"
 	end
 end
