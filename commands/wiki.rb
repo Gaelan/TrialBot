93550b78c747357d90746dc80261f734bbef68bb
Gaelan Steele <gbs@canishe.com>
2017-02-12 01:23:52 -0800

    try 2

--- commands/wiki.rb
+++ commands/wiki.rb
@@ -85,7 +85,12 @@ Bot.command :wdelete, required_permissions: [:manage_messages] do |event, name|
 end
 
 Bot.reaction_add do |event|
-	if !event.user.current_bot? && $entry_messages[event.message.id][event.emoji.name]
-		print_entry($entry_messages[event.message.id][event.emoji.name], event, true)
+	begin
+		if !event.user.current_bot? && $entry_messages[event.message.id][event.emoji.name]
+			print_entry($entry_messages[event.message.id][event.emoji.name], event, true)
+		end
+	rescue => e
+		require 'yaml'
+		puts e.to_yaml
 	end
 end
